e3be2540f14ca142b66492aec72f1681543a9c1b
Ilya <ikutuzov1702@gmail.com>
2022-02-02 11:37:07 +0300

    добавлен скрипт фильтрации данных по массиву

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+const arr = ['apple', ':)', 'bla', 'zerro', 'cocos', 'tom', 'dom'];
+
+const filter_arr = [];
+
+for(let i=0; i < arr.length; i++) {
+	if (arr[i].length <= 3) {
+		filter_arr.push(arr[i])
+	}
+};
+
+console.log('Filter data: ', filter_arr)
 
